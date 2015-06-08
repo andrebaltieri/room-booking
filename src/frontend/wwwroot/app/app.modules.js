dcb3c39944deb5cfd83ffc89642c4a96094a73d9
Andre Baltieri <andrebaltieri@hotmail.com>
2015-06-07 21:42:36 -0300

    Criado projeto web

--- src/frontend/wwwroot/app/app.modules.js
+++
@@ -1,5 +0,0 @@
-(function () {
-    'use strict';
-    angular.module('room.booking.directives', []);
-    angular.module('room.booking', ['ui.bootstrap', 'ngRoute', 'pascalprecht.translate', 'room.booking.directives']);
-})();
